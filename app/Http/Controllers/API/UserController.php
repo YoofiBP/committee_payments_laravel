3f2fc7c272dd40500f10e3377d925a8ea232e6a3
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-01-23 20:47:43 +0000

    First Commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function __construct(){
+        $this->middleware(['web','auth:sanctum'])->except(['login','store']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request)
+    {
+        return response()->json($request->user(),200);
+    }
+
+    /**
+     * Store a user in storage.
+     *
+     * @param  StoreUserRequest  $request
+     * @return JsonResponse
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create($data);
+        return response()->json(new UserResource($user));
+    }
+
+    /**
+     * Login existing user
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+        if(Auth::guard('web')->attempt($credentials, true)){
+            $request->session()->regenerate();
+           return response()->json(Auth::user(),200);
+        }else{
+            return response()->json(["message" => "Invalid credentials"],401);
+        }
+    }
+
+    /**
+     * Logout existing user
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request){
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json(["message" => "Successfully logged out"],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function show(User $user)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return void
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_number'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'is_verified',
+        'email_verified_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean'
+    ];
+
+    protected $attributes = [
+        'is_admin' => false,
+        'total_contribution' => 0
+    ];
+
+    public function setPasswordAttribute($value){
+        $this->attributes['password'] = password_hash($value, PASSWORD_DEFAULT);
+    }
+
+    public function setPhoneNumberAttribute($value){
+        $this->attributes['phone_number'] = trim(str_replace(' ', '', $value));
+    }
+}
